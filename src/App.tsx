94f8b297d98ecca0b08a53dafa6151e416bb7210
Mikael Escolin <mikaelescolin@gmail.com>
2021-08-26 16:00:13 +0100

    Add more counters to test performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
-import { ChakraProvider, Center, Flex } from '@chakra-ui/react';
+import { ChakraProvider, Flex, Box } from '@chakra-ui/react';
 import Counter1 from './counter/counter1.component';
 import Counter2 from './counter/counter2.component';
+
+const test: number[] = [];
+
+for (let i = 0; i < 100; i += 1) {
+  test.push(i);
+}
 
 const App: React.FunctionComponent = () => {
   return (
     <ChakraProvider>
-      <Center h="100vh" w="100vw">
-        <Flex w="75%">
-          <Counter1 />
-          <Counter2 />
-        </Flex>
-      </Center>
+      <Flex h="100vh" w="100vw" justifyContent="center" alignItems="center" flexWrap="wrap">
+        {test.map((elem) => (
+          <Box key={elem}>
+            <Flex flexDir="column">
+              <Flex w="75%">
+                <Counter1 />
+                <Counter2 />
+              </Flex>
+              <Flex w="75%">
+                <Counter1 />
+                <Counter2 />
+              </Flex>
+            </Flex>
+          </Box>
+        ))}
+      </Flex>
     </ChakraProvider>
   );
 };
